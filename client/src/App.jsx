227b9c63628d78c4c601b8fec0eb056ff4bb1639
Zhiyao Yu <z274yu@uwaterloo.ca>
2023-06-25 00:21:57 -0400

    Add Login&register, basic logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+// Import React router dom
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+// create a router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login/></div>
+  },
+  {
+    path: '/register',
+    element: <div><Register/></div>
+  }
+])
 
 function App() {
-
-
   return (
-    <div>
-      App
-    </div>
+   <div>
+     <RouterProvider router={router}/>
+   </div>
   )
 }
 
