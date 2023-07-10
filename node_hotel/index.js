d43168de1006b73563888792aa06bad257aa9360
Weilan Tao <twlemily@hotmail.com>
2023-07-09 20:17:35 -0400

    something new

--- node_hotel/index.js
+++ node_hotel/index.js
@@ -5,53 +5,64 @@ var amadeus = new Amadeus({
   clientSecret: 'JkFycaDtmBTXohCO'
 });
 
-amadeus.referenceData.locations.hotel.get({
-    keyword: 'PARI',
-    subType: 'HOTEL_GDS'
-}).then(function(response){
-  console.log(response.data[0]);
-  amadeus.shopping.hotelOffersSearch.get({
-    hotelIds: 'RTPAR001',
-    adults: '2'
-}).then((response)=>{
-    console.log(response)
-    
-    // amadeus.booking.hotelBookings.post(
-    //     JSON.stringify({
-    //       'offerId': 'RTPAR001',
-    //       'guests': [ {
-    //         "id": 1,
-    //         "name": {
-    //           "title": "MR",
-    //           "firstName": "BOB",
-    //           "lastName": "SMITH"
-    //         },
-    //         "contact": {
-    //           "phone": "+33679278416",
-    //           "email": "bob.smith@email.com"
-    //         }
-    //       }],
-    //       'payments': [{
-    //         "id": 1,
-    //         "method": "creditCard",
-    //         "card": {
-    //           "vendorCode": "VI",
-    //           "cardNumber": "4151289722471370",
-    //           "expiryDate": "2023-08"
-    //         }
-    //       }]
-    //       }
-    //     )
-    //   ).then((response)=>{
-    //     console.log(response)
-    //     }).catch(response=>{
-    //         console.log(response);
-    //     })
+const hotelList = [];
 
-  }).catch((response)=>{
-    console.log(response)
+function getHotelList() {
+  return new Promise((resolve, reject) => {
+    amadeus.referenceData.locations.hotel
+      .get({ keyword: 'PAR', subType: 'HOTEL_GDS' })
+      .then(function (response) {
+       
+        const data = response.data;
+        data.forEach((item) => {
+          hotelList.push(item.hotelIds[0]);
+        });
+        resolve(hotelList);
+      })
+      .catch(function (responseError) {
+        reject(responseError.code);
+      });
+  });
+}
+
+
+function getOffer(hotelIdVal){
+  return new Promise((resolve, reject)=>{
+    amadeus.shopping.hotelOffersSearch.get({
+      hotelIds: 'RTPAR001',
+      adults: '1',
+      'checkInDate': '2023-10-10',
+      'checkOutDate': '2023-10-12'
+
+  }).then(function (response) {
+    console.log(response.data[0]);
+  }).catch(function (response) {
+    console.error(response);
+  });
   })
-  
-}).catch(function(responseError){
-  console.log(responseError.code);
+}
+
+
+
+getHotelList()
+  .then((hotelList) => {
+    // console.log(hotelList); // Handle the hotelList array
+    console.log(hotelList[0]);
+     
+    amadeus.shopping.hotelOffersSearch.get({
+      hotelIds: 'RTPAR001',
+      adults: '2',
+      'checkInDate': '2023-10-10',
+      'checkOutDate': '2023-10-12'
+    }).then(function (response) {
+      console.log(response.data[0]);
+    }).catch(function (response) {
+      console.error(response);
+    });
+})
+  .catch((error) => {
+    console.log('Error:', error); // Handle the error
 });
+
+
+
