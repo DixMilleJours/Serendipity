2fa3d5c1114430edfe66cff5ca0bfac9d85a5dd9
De Nolan <t4zhai@edu.uwaterloo.ca>
2024-07-16 05:17:44 -0400

    Update

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -8,15 +8,13 @@ admin.initializeApp();
 const { Configuration, OpenAIApi } = require("openai");
 
 // Setup OpenAI API configuration with the fetched OpenAI key
-const configuration = new Configuration({
-    apiKey: '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',
-});
 
-// const placeAPIKey = '5ae2e3f221c38a28845f05b63e357b1b0a0ade8195a1ccd5ba27738b'
 
-// const yelpAPIKey = '-IdRLtv_5oNUULaCzsJ9GwDQTyV2YKgwlWgZT6v5hbniOgh0l0hEbQUCkacFvfZ_R6HPYTDx_hhZU0ewgrCC0vkYhLmvQ6qNDKS1hGe6nvc93mnBwJ5g1WzoCPC3ZHYx'
 
-const googleAPI = 'AIzaSyArF98C3uGVEnul-hfQwmZTRfPK8kmlzIs'
+
+
+
+
 
 const openai = new OpenAIApi(configuration);
 
@@ -29,163 +27,9 @@ const cors = require('cors')({ origin: true });
 const Amadeus = require('amadeus');
 
 /* Credentials start here... */
-var amadeus = new Amadeus({
-    hostname: 'production',
-    clientId: '7mjGX9eTRAs8VJe3JeAcylCiZBUQoRy0',
-    clientSecret: 'fwM8VVtfG05pEgzi'
-});
+
 
 /* Cloud functions start here... */
-// deprecated
-// exports.searchFlight = functions.https.onRequest(async (req, res) => {
-//     // ! Crucial, must use the cors wrapper.
-//     cors(req, res, () => {
-//         const TOKEN = 'duffel_test_jmmpvZzOOkD-qvaahFZGjjqtGXFcT8jipwFdcx-bIBH';
-//         var searchID = ''
-
-//         const data = req.body
-
-//         const headers = {
-//             'Accept-Encoding': 'gzip',
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json',
-//             'Duffel-Version': 'v1',
-//             'Authorization': `Bearer ${TOKEN}`
-//         }
-
-//         // First, find all flights satisfying user inputs.
-//         axios.post(
-//             'https://api.duffel.com/air/offer_requests?return_offers=false&supplier_timeout=10000', data, { headers })
-//             .then((res) => {
-//                 // Record the search id.
-//                 // ! Notice the first `data` is mandatory from Axios.
-//                 searchID = res.data.data.id;
-//                 // Then, get those found flights.
-//                 return axios.get('https://api.duffel.com/air/offers', {
-//                     params: {
-//                         // Use the recorded search id to fetch the entire list of eligible flights.
-//                         offer_request_id: searchID,
-//                         limit: 10,
-//                         sort: "total_amount",
-//                         max_connections: 0
-//                     },
-//                     headers
-//                 }
-//                 )
-//             }).then((response) => {
-//                 // ! Crucial, must set this.
-//                 res.set('Access-Control-Allow-Origin', '*');
-//                 res.send(response.data)
-//             })
-//             .catch((error) => {
-
-//             });
-//     })
-// });
-
-// exports.searchFlightV2 = functions.https.onRequest(async (req, res) => {
-//     cors(req, res, async () => {
-//         await amadeus.shopping.flightOffersSearch.get({
-//             // TODO - Get request from front-end.
-//             originLocationCode: 'YYZ',
-//             destinationLocationCode: 'JFK',
-//             departureDate: '2023-07-20',
-//             returnDate: '2023-07-24',
-//             adults: '1',
-//             children: '1',
-//             travelClass: 'ECONOMY',
-//             max: '10',
-//             nonStop: 'true'
-//         }).then((response) => {
-//             let flightRes = response.data.map(item => ({
-//                 carrier: item.validatingAirlineCodes[0],
-//                 currency: item.price.currency,
-//                 price: item.price.grandTotal,
-//                 go_duration: item.itineraries[0].duration,
-//                 go_departure: item.itineraries[0].segments[0].departure.iataCode,
-//                 go_departure_time: item.itineraries[0].segments[0].departure.at,
-//                 go_terminal: item.itineraries[0].segments[0].departure.terminal,
-//                 go_arrival: item.itineraries[0].segments[0].arrival.iataCode,
-//                 go_arrival_time: item.itineraries[0].segments[0].arrival.at,
-//                 leave_duration: item.itineraries[1].duration,
-//                 leave_departure: item.itineraries[1].segments[0].departure.iataCode,
-//                 leave_departure_time: item.itineraries[1].segments[0].departure.at,
-//                 leave_terminal: item.itineraries[1].segments[0].departure.terminal,
-//                 leave_arrival: item.itineraries[1].segments[0].arrival.iataCode,
-//                 leave_arrival_time: item.itineraries[1].segments[0].arrival.at
-//             }))
-//             res.set('Access-Control-Allow-Origin', '*');
-//             res.send(flightRes)
-//         }).catch((error) => {
-//             res.set('Access-Control-Allow-Origin', '*');
-//             res.send(error)
-//         });
-//     })
-// })
-
-// exports.searchHotels = functions.https.onRequest(async (req, res) => {
-//     cors(req, res, async () => {
-//         await amadeus.referenceData.locations.hotels.byCity.get({
-//             cityCode: 'JFK',
-//             ratings: "4",
-//             radius: "30"
-//         }).then(async (response) => {
-//             // Limit hotel count to 10.
-//             var limitedHotels = response.data.slice(0, 10);
-
-//             let hotelInfo = limitedHotels.map((hotel) => ({
-//                 hotelName: hotel.name,
-//                 hotelId: hotel.hotelId
-//             }))
-
-//             let hotelIds = hotelInfo.map((hotel) => hotel.hotelId);
-
-//             return await amadeus.shopping.hotelOffersSearch.get({
-//                 hotelIds: JSON.stringify(hotelIds),
-//                 adults: '1',
-//                 checkInDate: '2023-07-20',
-//                 checkOutDate: '2023-07-24',
-//                 roomQuantity: '1',
-//             })
-//         }).then((response) => {
-//             let hotelRes = response.data.map((hotel) => ({
-//                 hotelId: hotel.hotel.hotelId,
-//                 hotelName: hotel.hotel.name,
-//                 offerId: hotel.offers[0].id,
-//                 offerPrice: hotel.offers[0].price.total,
-//                 offerCurrency: hotel.offers[0].price.currency
-//             }))
-//             res.set('Access-Control-Allow-Origin', '*');
-//             res.send(hotelRes)
-//         }).catch((error) => {
-//             res.set('Access-Control-Allow-Origin', '*');
-//             res.send(error)
-//         });
-//     })
-// })
-
-// exports.getOptimalFlight = functions.https.onCall(async (data, context) => {
-//     const flightData = data.flightData;
-//     const budget = data.budget;
-
-//     // !!!! budget is an array of 2 numbers, so will need to change the prompt.
-//     const prompt = `Given the flight data ${JSON.stringify(flightData)} and a budget of ${budget}, return the optimal flight in human-readable sentence. No explanation.`;
-
-//     try {
-//         const gptResponse = await openai.createChatCompletion({
-//             model: 'gpt-3.5-turbo',
-//             messages: [
-//                 {
-//                     role: 'user',
-//                     content: prompt
-//                 }
-//             ]
-//         })
-//         return { gptResponse: gptResponse.data.choices[0].message }
-//     } catch (error) {
-//         return { error: 'An error occurred while processing your request.' };
-//     }
-// });
 
 async function searchHotels(data) {
     try {
